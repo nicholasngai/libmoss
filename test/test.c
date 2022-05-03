b33b0d6f559929c923a7745f3cf43d62ca668db6
Nicholas Ngai <nicholas@ngai.me>
2022-05-03 08:58:03 -0700

    Move winnow testing to separate function

--- test/test.c
+++ test/test.c
@@ -1,17 +1,22 @@
+#include <assert.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 #include <libmoss/winnowing.h>
 
-int main(void) {
-    uint64_t hashes[] = {77, 74, 42, 17, 98, 50, 17, 98, 8, 88, 67, 39, 77, 74, 42, 17, 98};
+static void test_winnow(void) {
+    static const uint64_t hashes[] = {
+        77, 74, 42, 17, 98, 50, 17, 98, 8, 88, 67, 39, 77, 74, 42, 17, 98
+    };
+    static const uint64_t expected[] = { 17, 17, 8, 39, 17 };
+    uint64_t result[5];
+    size_t result_len = 0;
     moss_winnow_t winnow;
     int ret;
 
     ret = moss_winnow_init(&winnow, 4);
-    if (ret) {
-        goto exit;
-    }
+    assert(!ret);
 
     size_t hashes_read;
     size_t start = 0;
@@ -22,12 +27,18 @@ int main(void) {
                 sizeof(hashes) / sizeof(*hashes) - start, &fingerprint);
         start += hashes_read;
         if (hashes_read) {
-            printf("%lu %lu\n", start, fingerprint);
+            assert(result_len < 5);
+            result[result_len] = fingerprint;
+            result_len++;
         }
     } while (hashes_read);
 
-    moss_winnow_free(&winnow);
+    assert(result_len == 5);
+    assert(!memcmp(result, expected, sizeof(result)));
 
-exit:
-    return ret;
+    moss_winnow_free(&winnow);
+}
+
+int main(void) {
+    test_winnow();
 }
