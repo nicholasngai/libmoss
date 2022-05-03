4f4d9515e1a146eb43e2a5da11d7c0be09f30778
Nicholas Ngai <nicholas@ngai.me>
2022-05-02 23:46:08 -0700

    Handle init errors in test

--- test/test.c
+++ test/test.c
@@ -6,21 +6,28 @@
 int main(void) {
     uint64_t hashes[] = {77, 74, 42, 17, 98, 50, 17, 98, 8, 88, 67, 39, 77, 74, 42, 17, 98};
     moss_winnow_t winnow;
+    int ret;
 
-    moss_winnow_init(&winnow, 4);
+    ret = moss_winnow_init(&winnow, 4);
+    if (ret) {
+        goto exit;
+    }
 
-    size_t ret;
+    size_t hashes_read;
     size_t start = 0;
     do {
         uint64_t fingerprint;
-        ret =
+        hashes_read =
             moss_winnow_process(&winnow, hashes + start,
                 sizeof(hashes) / sizeof(*hashes) - start, &fingerprint);
-        start += ret;
-        if (ret) {
+        start += hashes_read;
+        if (hashes_read) {
             printf("%lu %lu\n", start, fingerprint);
         }
-    } while (ret);
+    } while (hashes_read);
 
     moss_winnow_free(&winnow);
+
+exit:
+    return ret;
 }
