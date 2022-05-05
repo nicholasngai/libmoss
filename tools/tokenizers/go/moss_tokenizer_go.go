c03e98c16a74b076c71453865bc776d299652aa4
Nicholas Ngai <nicholas@ngai.me>
2022-05-05 10:38:47 -0700

    Pass filename to tokenizer on CLI

--- tools/tokenizers/go/moss_tokenizer_go.go
+++ tools/tokenizers/go/moss_tokenizer_go.go
@@ -15,13 +15,19 @@ func panicIfErr(err error) {
 }
 
 func main() {
-	data, err := ioutil.ReadAll(os.Stdin)
+	if len(os.Args) < 2 {
+		fmt.Println("usage: " + os.Args[0] + " file")
+		os.Exit(-1)
+	}
+
+	filename := os.Args[1]
+	data, err := ioutil.ReadFile(filename)
 	panicIfErr(err)
 
 	var s scanner.Scanner
 
 	fset := token.NewFileSet()
-	file := fset.AddFile("file.go", fset.Base(), len(data))
+	file := fset.AddFile(filename, fset.Base(), len(data))
 	s.Init(file, data, nil, 0)
 
 	for {
